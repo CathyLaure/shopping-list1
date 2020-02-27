a8b4ff9bbb98f6a2819d1c87fc32f5b6aad2b7c1
Cathy Laure Tasama <ctasama@cnm.edu>
2020-02-27 12:38:33 -0800

    added shopping-list1 page

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit", (event) => {
+        handleItemForm(event, form);
+    });
+}
+
+function handleItemForm(event, fromRef) {
+    if(event.preventDefault()) {
+        event.preventDefault();
+    }
+
+    console.log("Event: ", event);
+
+    return false;
+}
+/*addItemToShoppingList();
+   */
+/*function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+        console.log("Item HTML: ", itemHtml);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
+}
+
+function createListItemHtml(itemName, itemAmount) {
+    return `<li>
+                ${itemName} - ${itemAmount}
+                <button type="button">Delete Item</button>
+            </li>`;
+}
+*/
